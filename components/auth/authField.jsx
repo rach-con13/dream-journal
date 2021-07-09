ae06977b0d8857a507d6a7489035e2b4158a979c
rach-con13 <rachaelconcessio+@gmail.com>
2021-07-09 17:29:47 -0400

    test commit

--- components/auth/authField.jsx
+++ components/auth/authField.jsx
@@ -1,6 +1,7 @@
 // input field component for user signup and login. 
 import React from 'react'
 import style from "./field.module.css";
+import PropTypes from "prop-types";
 export default function AuthField(props) {
     
     
@@ -10,4 +11,11 @@ export default function AuthField(props) {
         <input className={style.inputField} id={props.name} name={props.name} type={props.type} onChange={props.onChange} value={props.value} />
     </div>
     )
+}
+AuthField.propTypes = {
+    label:PropTypes.string,
+    name:PropTypes.string,
+    type:PropTypes.string,
+    onChange:PropTypes.func,
+    value:PropTypes.string
 }
--- components/editor/editor.jsx
+++ components/editor/editor.jsx
@@ -1,11 +1,11 @@
 import next from 'next';
 import PropTypes from "prop-types";
 import React, { useCallback, useEffect, useMemo, useState } from 'react'
-import {createEditor,Editor, Transforms,Text,Value,Selection,Range} from "slate";
-import {Slate,Editable,withReact} from "slate-react";
+import { createEditor, Editor, Transforms, Text, Value, Selection, Range } from "slate";
+import { Slate, Editable, withReact } from "slate-react";
 import EditorToolbar from "./editorToolbar";
-import {Leaf} from "../../lib/slate/leaf";
-import { CodeElement,QuoteElement,ImageElement,DefaultElement } from './elementBlocks';
+import { Leaf } from "../../lib/slate/leaf";
+import { CodeElement, QuoteElement, ImageElement, DefaultElement } from './elementBlocks';
 import { withImages } from './plugins/withImage';
 
 
@@ -13,69 +13,68 @@ export default function EditorSection(props) {
     const [editor] = useState(() => withImages(withReact(createEditor())))
     // const editor = useMemo(() => withReact(createEditor()), [])
     const [value, setValue] = useState([
-      {
-        type: 'paragraph',
-        children: [{ text: 'A line of text in a paragraph.' }],
-      },
+        {
+            type: 'paragraph',
+            children: [{ text: 'A line of text in a paragraph.' }],
+        },
     ])
-    
-   const renderElement = useCallback(props => {
 
-       switch(props.element.type) {
-           case 'code':
-               return <CodeElement {...props} />
-           case "quote":
-               return <QuoteElement {...props} />
-           case "image":
-               return <ImageElement {...props} />
-           default:
-               return <DefaultElement {...props} />
-       }
-   },[])
-   
-   const renderLeaf = useCallback(props => <Leaf {...props} />, [])
+    const renderElement = useCallback(props => {
+
+        switch (props.element.type) {
+            case 'code':
+                return <CodeElement {...props} />
+            case "quote":
+                return <QuoteElement {...props} />
+            case "image":
+                return <ImageElement {...props} />
+            default:
+                return <DefaultElement {...props} />
+        }
+    }, [])
+
+    const renderLeaf = useCallback(props => <Leaf {...props} />, [])
 
     return (
         <div>
-          
-            <Slate  editor={editor}
-            value={value} 
-      
-            onChange={(newValue) => {
-                setValue(newValue);
-         
-            }}>
-              
-                <EditorToolbar editor={editor} /> 
-                <div className="mt-8 mx-auto w-10/12  ">
+
+            <Slate editor={editor}
+                value={value}
+
+                onChange={(newValue) => {
+                    setValue(newValue);
+
+                }}>
+
+                <EditorToolbar editor={editor} />
+                <div className="w-10/12 mx-auto mt-8 ">
                     <Editable renderLeaf={renderLeaf} renderElement={renderElement} onKeyDown={e => {
-                        if(!e.ctrlKey) {
+                        if (!e.ctrlKey) {
                             return;
                         }
                         switch (e.key) {
                             case '`': {
-                              e.preventDefault()
-                              const [match] = Editor.nodes(editor, {
-                                match: n => n.type === 'code',
-                              })
-                              Transforms.setNodes(
-                                editor,
-                                { type: match ? null : 'code' },
-                                { match: n => Editor.isBlock(editor, n) }
-                              )
-                              break
+                                e.preventDefault()
+                                const [match] = Editor.nodes(editor, {
+                                    match: n => n.type === 'code',
+                                })
+                                Transforms.setNodes(
+                                    editor,
+                                    { type: match ? null : 'code' },
+                                    { match: n => Editor.isBlock(editor, n) }
+                                )
+                                break
                             }
                         }
                     }} />
                 </div>
-            
+
             </Slate>
-           
+
         </div>
     )
 }
 
-
 EditorSection.propTypes = {
-    element:PropTypes.object
+    element: PropTypes.object
 }
